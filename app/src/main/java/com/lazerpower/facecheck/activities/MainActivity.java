cc7359476b68c8bd3cc8ed01ad9c8c57db5e0e2e
niraj-rayalla <niraj.rayalla@gmail.com>
2015-04-03 23:13:43 -0500

    Added an empty main activity and added library dependencies that will be needed.

---
+++ app/src/main/java/com/lazerpower/facecheck/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lazerpower.facecheck.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import com.lazerpower.facecheck.R;
+
+/**
+ * Created by Niraj on 4/3/2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
